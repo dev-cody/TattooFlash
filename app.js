3ca6db6cc341217de169e3e4cad733292fb3a7f0
Cody <codykoscielski@gmail.com>
2021-01-03 17:26:31 -0500

    First commit of the project. Created a fake JSON server to pull paiting information as if it was a database.

---
+++ app.js
@@ -0,0 +1,61 @@
+//Nav Bar
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+//Market place DIV
+const market = document.querySelector('.paitings-container');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+});
+
+//Get the products from the list
+class Paitings{
+  async getProducts() {
+    try {
+      let result = await fetch("paitings.json");
+      let data = await result.json();
+      let paitings = data.paitings;
+      return paitings;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+class UI {
+  displayPaitings(paitings) {
+    let result = '';
+    paitings.forEach( paiting => {
+      result += `
+        <div class="paiting-card" key=${paiting.id}>
+          <img src=${paiting.url} alt=${paiting.title}>
+          <div class="card-body">
+              <h3>${paiting.title}</h3>
+              <p>Artist: ${paiting.artist}</p>
+          </div>
+          <div class="card-sale">
+              <h3>${paiting.price}</h3></h3>
+              <button class="btn">Buy</button>
+          </div>
+        </div>
+      `
+    });
+    market.innerHTML = result;
+  }
+}
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  //Make sure that the API is only fetched on market.thml
+  if(window.location.pathname === '/market.html') {
+    const products = new Paitings();
+    const ui = new UI();
+    //Get all of the products
+    products.getProducts().then(paitings => ui.displayPaitings(paitings));
+  }
+
+});
+
+
